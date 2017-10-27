ed219e5a8259e7abbd0757b1b519c0faca7ea944
Thomas Rzipa <t.rzipa@adrom.at>
2017-10-27 12:45:07 +0200

    fix mongodb url

--- src/server.js
+++ src/server.js
@@ -1,24 +1,24 @@
 var express = require('express')
 var shortid = require('shortid')
-var mongoUrl = process.env.MONGOLAB_URI;
+var mongoUrl = "mongodb";
 var db = require('monk')(mongoUrl)
 var links = db.get('links');
 
 var app = express()
-var baseUrl = "https://rumpels-fcc-url-shortener-ms.herokuapp.com/"
+var baseUrl = "http://shortener.app.rzipa.at/"
 
 app.use(express.static('public'));
 
 app.get('/new/*', function (req, res) {
-    var url = req.params[0]
-    var key = shortid.generate()
-    links.insert({ key: key, url: url })
-    res.send(JSON.stringify({ "original_url": url, "short_url": baseUrl+key}))
+  var url = req.params[0]
+  var key = shortid.generate()
+  links.insert({ key: key, url: url })
+  res.send(JSON.stringify({ "original_url": url, "short_url": baseUrl + key }))
 })
 
 app.get('/:shortUrl', function (req, res) {
   var shorturl = req.params.shortUrl
-  var url = links.findOne({ key: shorturl}).then((doc)=>{
+  var url = links.findOne({ key: shorturl }).then((doc) => {
     res.setHeader('Content-Type', 'application/json')
     res.redirect(doc.url);
   }).catch((err) => {
@@ -28,5 +28,5 @@ app.get('/:shortUrl', function (req, res) {
 
 var port = process.env.PORT || 8080
 app.listen(port, function () {
-  console.log('App listening on port '+port)
+  console.log('App listening on port ' + port)
 })
