968f59b83677a5dc39687fd37ef4689276c05083
Thomas Rzipa <t.rzipa@adrom.at>
2017-10-31 15:21:07 +0100

    Update to pug

--- src/server.js
+++ src/server.js
@@ -5,15 +5,27 @@ var db = require('monk')(mongoUrl)
 var links = db.get('links');
 
 var app = express()
-var baseUrl = "http://shortener.app.rzipa.at/"
 
 app.use(express.static('public'));
+
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+  var baseUrl = req.protocol + '://' + req.hostname + ':' + port;
+
+  var mainDomain = req.hostname.substr(req.hostname.indexOf('.') + 1);
+  var baseHome = req.protocol + '://' + mainDomain + ':' + port
+
+  res.render('index', { host: baseUrl, home: baseHome });
+});
 
 app.get('/new/*', function (req, res) {
   var url = req.params[0]
   var key = shortid.generate()
+  var baseUrl = req.protocol + '://' + req.hostname + ':' + port;
+
   links.insert({ key: key, url: url })
-  res.send(JSON.stringify({ "original_url": url, "short_url": baseUrl + key }))
+  res.send(JSON.stringify({ "original_url": url, "short_url": baseUrl + '/' + key }))
 })
 
 app.get('/:shortUrl', function (req, res) {
