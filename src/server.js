0a43572d5cd4be136d11556a0511524c4f65f651
Thomas Rzipa <t.rzipa@adrom.at>
2018-06-13 08:46:29 +0200

    another fix

--- src/server.js
+++ src/server.js
@@ -12,16 +12,18 @@ app.set('view engine', 'pug');
 
 var mainRouter = express.Router();
 var basePath = process.env.BASEPATH || '';
-var baseUrl = req.protocol + '://' + req.hostname;
 app.use(basePath, mainRouter);
 
 mainRouter.get('/', function (req, res) {
+  var baseUrl = req.protocol + '://' + req.hostname;
   res.render('index', { baseUrl, basePath });
 });
 
 mainRouter.get('/new/*', function (req, res) {
   var url = req.params[0]
   var key = shortid.generate()
+  var baseUrl = req.protocol + '://' + req.hostname;
+  
   links.insert({ key: key, url: url })
   res.send(JSON.stringify({ "original_url": url, "short_url": baseUrl + basePath + '/' + key }))
 })
